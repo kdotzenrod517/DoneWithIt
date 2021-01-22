a24cb1116cf6e1a5e18286270c34dbd5598d77d1
Krista Dotzenrod <kdotzenrod517@gmail.com>
2021-01-22 09:42:50 -0600

    Playing with touchable

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
 
-  console.log("App executed!");
-
+  const handlePress = () => console.log("Text Pressed!");
+  
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native</Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+      <Image
+      source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300" }}/>
+      </TouchableNativeFeedback>
+
     </View>
   );
 }
@@ -19,6 +26,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
